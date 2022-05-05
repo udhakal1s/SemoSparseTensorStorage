a9f640d0ba1a326b775aa72198118bb7e1437824
Truskiwich <44007689+Truskiwich@users.noreply.github.com>
2022-05-04 22:44:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,21 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print(round(x))
+    HiCOO.fileToHiCOO('venv/tensor1.tn')
+    print('ei array = ', HiCOO.ielements)
+    print('ej array = ', HiCOO.jelements)
+    print('ek array = ', HiCOO.kelements)
+    print('bi array = ', HiCOO.iblock)
+    print('bj array = ', HiCOO.jblock)
+    print('bk array = ', HiCOO.kblock)
+    print('Values array = ', HiCOO.values)
+    print('Block Pointer array = ', HiCOO.bptr)
+    HiCOO.tosuperblock()
+    print()
+    HiCOO.printsuperblock()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
